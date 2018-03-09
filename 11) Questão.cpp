2e93728ac88369c0d77038d63c10f3421286da00
Kaio Silveira <35278957+kaiosilveira89@users.noreply.github.com>
2018-03-09 13:53:57 -0300

    Update 11) Questão.cpp
    Variável quantidade foi modificada de "float" para "int". A operação aritmética não apresentou erro.

--- 11) Questão.cpp
+++ 11) Questão.cpp
@@ -3,41 +3,50 @@
 #include<locale.h>
 
 int main(){
-	setlocale(LC_ALL, "Portuguese");
-	char nomeProduto;
-	float quantidade;
-	float preco;
-	float total;
-	float desconto5 = total*0.02;
-    float desconto9 = total*0.03;
-    float desconto10 = total*0.05;
-    float totalPagar;
+setlocale(LC_ALL, "Portuguese");
 	
-	printf("Digite o nome do produto: \n");
-	scanf("%c", &nomeProduto);
+char produto[20];
+int quantidade;
+float preco;
+float total;
+float desconto5;
+float desconto9;
+float desconto10;
+float totalPagar;
 	
-	printf("Digite o preço do produto: \n");
-	scanf("%f", &preco);
+total = preco*quantidade;
+desconto5 = total*0.02;
+desconto9 = total*0.03;
+desconto10 = total*0.05;
 	
-	printf("Digite a quantidade que deseja adquirir: \n");
-	scanf("%f", &quantidade);
+printf("--------------------------------\n");
+		
+printf("Digite o nome do produto: ");
+scanf("%s", &produto); 
 	
-	total = preco*quantidade;
+printf("Digite a quantidade que deseja adquirir: ");
+scanf("%d", &quantidade);
 	
-	if (quantidade > 5){
-            if ( quantidade <= 10){
-                totalPagar=total-desconto9;
-                printf("Total a pagar por %0.0f unid. de %c: \n %0.2f", quantidade, nomeProduto, totalPagar);
-                }
-            else {
-                totalPagar=total-desconto10;
-                printf("Total a pagar por %0.0f unid. de %c: \n %0.2f", quantidade, nomeProduto, totalPagar);
-            }
-	    }
-	else {
-	    totalPagar=total-desconto5;
-        printf("Total a pagar por %0.0f unid. de %c: \n %0.2f", quantidade, nomeProduto, totalPagar);
-		}
+printf("Digite o preço do produto: R$");
+scanf("%f", &preco);
+	
+printf("--------------------------------\n");
+	
+   if (quantidade > 5){
+       	if (quantidade > 10){
+	   	totalPagar = total - desconto10;
+            	printf("Total a pagar por %d unid. de %s: R$%.2f.\nSeu desconto foi de: R$%.2f", quantidade, produto, totalPagar, desconto10);
+         	}
+
+   	else {
+		totalPagar = total - desconto9;
+    		printf("Total a pagar por %d unid. de %s: R$%.2f.\nSeu desconto foi de: R$%.2f", quantidade, produto, totalPagar, desconto9);
+ 		}
+	}
+   else	{
+	totalPagar = total - desconto5;
+        printf("Total a pagar por %d unid. de %s: R$%.2f.\nSeu desconto foi de: R$%.2f", quantidade, produto, totalPagar, desconto5);
+	}
 
 	return 0;
 }
