a457e6a6fd2be48efc9732f44eb7feba6d6d8742
Kaio Silveira <35278957+kaiosilveira89@users.noreply.github.com>
2018-03-11 21:03:13 -0300

    Update 19) Questão.cpp
    Resolvido problema da busca do maior preço! Ao invés de comparar a primeira posição com a seguinte (como é feita nas ordenações), foi criado uma variável para ler o vetor inteiro, de preco[1] até preco[15]), e guardar o maior vetor encontrado na variável float maiorPreco.

--- 19) Questão.cpp
+++ 19) Questão.cpp
@@ -6,28 +6,24 @@ int main(){
 	setlocale(LC_ALL, "Portuguese");
     char produto[15] = {0};
     float preco[15] = {0};
+    float maiorPreco;
     float total = 0;
     float media = 0;
     int i, j;
-    float aux = 0;
     
     //incrementando vetores
     for(i = 1; i < 16; i++){
-        printf("Digite o nome do %d° produto: \n", i);
+        printf("Digite o nome do %i° produto: \n", i);
         scanf("%s", &produto[i]);
-        printf("Digite o preço do %d° produto: \n", i);
+        printf("Digite o preço do %i° produto: \n", i);
         scanf("%f", &preco[i]);
     }
 
     //busca maior preço
-    for(i = 1; i < 16; i++)
-    for(j = i+1; j < 16; j++){
-        if (preco[i]>preco[j]){
-            aux = preco[i];   
-        }
-        else
-        {
-            aux = preco[j];
+    maiorPreco = preco[1];
+    for(i = 1; i < 16; i++){
+        if (maiorPreco < preco[i]){
+            maiorPreco = preco[i];
         }
     }
     
@@ -37,12 +33,11 @@ int main(){
     }
 
     media = total/15;
-
     
     //saida de dados
     
     printf("O preço total é: R$%.2f\n", total);
-    printf("O maior preço encontrado foi: R$%.2f.\n", aux);
+    printf("O maior preço encontrado foi: R$%.2f.\n", maiorPreco);
     printf("O preço médio é: R$%.2f\n", media);
 
 	return 0;
